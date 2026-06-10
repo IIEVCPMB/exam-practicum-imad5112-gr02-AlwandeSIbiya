825c5186327f139dac4b97a854333afb22d77fbf
AlwandeSIbiya <st10479148@myemeris.edu.za>
2026-06-10 10:17:30 +0200

    added varialbles,
    code for what i want the buttons to do and how i want the text to appear with suggestions

--- app/src/main/java/com/example/campsitecommander/MainActivity.kt
+++ app/src/main/java/com/example/campsitecommander/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.campsitecommander
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,14 +19,31 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.campsitecommander.ui.theme.CampsiteCommanderTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            ->
             setContentView(R.layout.main_layout)
 
             //Link UI components to variables
-            val inputItem: find
+            val inputItem = findViewById<EditText>(R.id.additemstxt)
+            val btngear = findViewById<Button>(R.id.btngear)
+            val txtResult= findViewById<TextView>(R.id.txtResult)
+
+            btngear.setOnClickListener {
+                val item = inputItem.text.toString().trim().lowercase()
+                if(item.isEmpty()) {
+                    Toast.makeText(this,"Add gear you like", Toast.LENGTH_SHORT). show()
+                    return@setOnClickListener
+                }
+
+
+            }
+
+
+
 
 
         }
