e64befef3fb18655aa4a5def44fc23ba5ffa0d72
AlwandeSIbiya <st10479148@myemeris.edu.za>
2026-06-10 09:30:15 +0200

    changes to main

--- app/src/main/java/com/example/campsitecommander/MainActivity.kt
+++ app/src/main/java/com/example/campsitecommander/MainActivity.kt
@@ -19,30 +19,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             setContentView(R.layout.main_layout)
-            CampsiteCommanderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+            //Link UI components to variables
+            val inputItem: find
+
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CampsiteCommanderTheme {
-        Greeting("Android")
     }
 }
